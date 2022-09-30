d5c10037a450ed994482017cb2274e87f7e4a8cd
mariopc1 <90729724+mariopc1@users.noreply.github.com>
2022-09-30 12:29:39 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
    public static void main(String [] args){
-         System.out.println("**************MADE SOME CHANGES****************");
+         System.out.println("**************MADE SOME CH*ANGES****************");
     }
 }
